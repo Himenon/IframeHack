26a7db85649430654a50f1e493fa6ea88923e5ea
Himenon <k.himeno314@gmail.com>
2018-11-01 13:34:17 +0900

    update(postMesageを使った

--- LocalStorage/inner.js
+++ LocalStorage/inner.js
@@ -1,1 +1,24 @@
-console.log("inner,js");
+// @ts-check
+
+window.addEventListener('message', function(event) {
+  this.console.log("8005はイベントを受信しました: ", event);
+  if (event.data) {
+    if (event.data.type === "save") {
+      console.log("http://localhost:8005/inner.htmlへ書き込み");
+      saveData("params", event.data.body.params);
+    }
+    if (event.data.type === "read") {
+      console.log("http://localhost:8005/inner.htmlから読み込み");
+      const data = readData("params");
+      event.source.postMessage(data, event.origin);
+    }
+  }
+}, false);
+
+function saveData(key, value) {
+  window.localStorage.setItem(key, value);
+}
+
+function readData(key) {
+  return window.localStorage.getItem(key);
+}
--- LocalStorage/main.js
+++ LocalStorage/main.js
@@ -1,1 +1,33 @@
-console.log("hello");
+// @ts-check
+
+function writeData() {
+  const target = document.getElementById("target-iframe");
+  if (target) {
+    console.info(target.src, "に対して、localStorageに書き込むようにメッセージを送信します")
+    target.contentWindow.postMessage({
+      type: "save",
+      body: {
+        params: ["data1", "data2"]
+      }
+    }, target.src);
+  }
+}
+
+function readData() { 
+  const target = document.getElementById("target-iframe");
+  if (target) {
+    console.info(target.src, "に対して、localStorageから読み込むようにメッセージを送信します")
+    target.contentWindow.postMessage({
+      type: "read"
+    }, target.src);
+  }
+}
+
+window.onload = () => {
+  document.getElementById("local-write").addEventListener("click", writeData);
+  document.getElementById("local-read").addEventListener("click", readData);
+}
+
+window.addEventListener('message', function(event) {
+  console.info("8004はイベントを受信しました: ", event);
+});
